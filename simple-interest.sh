471295bd984926ec6509be7545a0bf6ed3aaaf01
meadeus <ghostnames@mail.ru>
2026-02-10 18:38:20 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -8,3 +8,10 @@
 # Calculates simple interest using formula:
 # Interest = (Principal * Rate * Time) / 100
 
+set -e  # Exit on error
+
+# Color codes for better output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color

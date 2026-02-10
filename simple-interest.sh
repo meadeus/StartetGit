b99546f21020b9ccde4f762e27824061ed2cbd04
meadeus <ghostnames@mail.ru>
2026-02-10 18:37:55 +0300

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Simple Interest Calculator
+# Author: [Your Name]
+# Date: $(date +%Y-%m-%d)
+# Version: 1.0
+#
+# Calculates simple interest using formula:
+# Interest = (Principal * Rate * Time) / 100
+
